e50d2bafb8ea864626f08bad77c0b3771ff3be8f
Tharaka Jayalath <tharakajayalath@Tharakas-MacBook-Pro.local>
2025-04-21 16:17:47 -0400

    second

--- Codes/00.Run All.R
+++ Codes/00.Run All.R
@@ -1,33 +1,11 @@
 
-# notes on files/folders
-
-# split data folder into rawdata and deriveddata folders
-
-# what is the PilotData folder? 
-#     maybe move to rawdata
-
-# load all packages in this initial script
-
-# ensure file paths start from the R project.  
-#     For example in the 01 script, the first line gives an error because it uses paths unique to your computer.
-#       df <- read_sav("~/Library/CloudStorage/OneDrive-UniversityofSaskatchewan/Chapter III-UseNonUseValue/R Project/Data/Water_Quality.sav")
-#       Error: '~/Library/CloudStorage/OneDrive-UniversityofSaskatchewan/Chapter III-UseNonUseValue/R Project/Data/Water_Quality.sav' does not exist.
-
-#     This can be fixed (and simplified to).
-#       df <- read_sav("Data/Water_Quality.sav")
-
-# For models 4 and 5, should the opt-out alternative variable
-#   "b_wq_nonlocal_sub_basin*WQ_SUBBASIN_NL_POLICY" be "b_wq_nonlocal_sub_basin*WQ_SUBBASIN_NL_CURRENT"
-
-# Can you also provide a summary of question Q17 for the main WQ survey and choose politics, movie, and WQ sample?
-#     For each statement provide a comparison barplot for with each treatment side by side.
-
 # Chapter III - Public survey pilot data analysis
 
 library(pacman)
 library(tidyverse)
 library(apollo)
 library(car)
+library(haven)
 
 ## create supoortig data files to set the choice dataset for Apollo
 source("./codes/01.Creating Supporting Dataframe.R")
